e4e53f5b67225cfa52cc39d2f58854f8cc1f6f91
Jonathan Dursi <jdursi@nvidia.com>
2025-05-28 14:28:03 -0400

    Modify rwt to take # of processes

--- workspace/code/run_w_torchrun.sh
+++ workspace/code/run_w_torchrun.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+if [ $# -lt 2 ]
+then
+    echo >&2 "ERROR: not enough arguments provided"
+    echo >&2 ""
+    echo >&2 "Usage: $0 number-of-processes script-to-run [arg1 arg2...]"
+    echo >&2 "       e.g. $0 4 ./arguments.py a b c 1233"
+    exit 1
+else
+    nproc_per_node=$1
+    shift 
+fi
+
 # this identifies the run. It can be anything,
 # but if you're running torchrun across multiple nodes,
 # it has to be the same for each of them
@@ -17,7 +29,7 @@ rendezvous_server=localhost
 
 OMP_NUM_THREADS=1 \
 torchrun --nnodes 1 \
-         --nproc_per_node 4 \
+         --nproc_per_node ${nproc_per_node} \
          --rdzv_id ${id} \
          --rdzv_backend c10d \
          --rdzv_endpoint ${rendezvous_server}:29500 \
