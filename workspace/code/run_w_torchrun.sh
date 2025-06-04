16a49c59cc5bb962b2d44ae659b5750d17f6bafc
Jonathan Dursi <jonathan@dursi.ca>
2025-06-04 16:40:19 -0400

    broader range of torchrun examples

--- workspace/code/run_w_torchrun.sh
+++ workspace/code/run_w_torchrun.sh
@@ -12,25 +12,9 @@ else
     shift 
 fi
 
-# this identifies the run. It can be anything,
-# but if you're running torchrun across multiple nodes,
-# it has to be the same for each of them
+NTHREADS=4 # should be set up to be number of CPU cores / number of processes, generally
 
-id=$RANDOM
-
-# identifies one of the torchrun servers as the one
-# to use for coordination.
-# Here there's just one node, so we're using localhost
-# but if you're running torchrun across multiple nodes,
-# all the nodes have to agree; often the first node 
-# (in, e.g., SLURM_JOB_NODELIST) is chosen
-
-rendezvous_server=localhost
-
-OMP_NUM_THREADS=1 \
-torchrun --nnodes 1 \
+OMP_NUM_THREADS=${NTHREADS} \
+torchrun --standalone \
          --nproc_per_node ${nproc_per_node} \
-         --rdzv_id ${id} \
-         --rdzv_backend c10d \
-         --rdzv_endpoint ${rendezvous_server}:29500 \
          $@
